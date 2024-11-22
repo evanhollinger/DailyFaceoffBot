34f678d58c6a2f541f338f580d2feca23015b090
evanhollinger <evan.hollinger.23@cnu.edu>
2024-11-21 23:39:51 -0500

    Day 0: Hello World

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+
+if __name__ == '__main__':
+    # This project will be to create a bot that monitors L1/PP1 changes for each team in the NHL
+    # Taking data from the url: https://www.dailyfaceoff.com/teams
+
+    print("Hello World")
